4e15ce61ce338268a55a6d00f5529faa241ac4ee
Dave Cusatis <davecusatis1@gmail.com>
2018-09-22 19:32:02 -0700

    add image uploader component and use it on the broadcaster page

---
+++ src/components/image-uploader/component.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import './component.scss';
+
+const ALLOWED_FORMATS = ['image/png', 'image/jpg', 'image/jpeg'];
+
+interface PublicProps {
+  assetName: string;
+}
+
+interface State {
+  error: string;
+  loadedImage: HTMLImageElement;
+}
+
+export class ImageUploader extends React.Component<PublicProps, State> {
+  constructor(props: PublicProps) {
+    super(props);
+    this.state = {
+      error: '',
+      loadedImage: new Image(),
+    }
+  }
+
+  private handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+
+    let loadedImg = new Image();
+    const img = e.currentTarget.files[0];
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      if (img.size > 25000) {
+        this.setState({
+          error: 'Exceeds size limits',
+        });
+        return;
+      }
+
+      if (ALLOWED_FORMATS.indexOf(img.type) === -1) {
+        this.setState({
+          error: `Format: ${img.type} not allowed.`,
+        });
+        return;
+      }
+
+      loadedImg.onload = () => {
+        if (loadedImg.width < 0 || loadedImg.width > 128) {
+          this.setState({
+            error: 'Image width is greater than the allowed limit of 128px',
+          });
+          return;
+        }
+        if (loadedImg.height < 0 || loadedImg.height > 128) {
+          this.setState({
+            error: 'Image height is greater than the allowed limit of 128px',
+          });
+          return;
+        }
+        this.setState({
+          loadedImage: loadedImg,
+        });
+      };
+      loadedImg.src = reader.result as string;
+    };
+    reader.readAsDataURL(img);
+    this.setState({
+      error: '',
+    })
+  }
+
+  public render() {
+    const { error, loadedImage } = this.state;
+
+    return (
+      <div className='uploader-container'>
+        {loadedImage.src &&
+          <div>
+            <img
+              className='image-preview'
+              src={loadedImage.src} />
+          </div>}
+
+        <input className='file-input'
+          type='file'
+          onChange={(e) => this.handleChange(e)} />
+
+        {error &&
+          <div className='upload-errors'>
+            {error}
+          </div>}
+      </div>
+    );
+  }
+}
--- src/pages/broadcaster/component.tsx
+++ src/pages/broadcaster/component.tsx
@@ -2,6 +2,17 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { API } from '../../core/api';
 import { Session } from '../../core/models/session';
+import { ImageUploader } from '../../components/image-uploader';
+import * as bar from '../../assets/img/iconBar.png';
+import * as bell from '../../assets/img/iconBell.png';
+import * as cherries from '../../assets/img/iconCherries.png';
+import * as coin from '../../assets/img/iconCoin.png';
+import * as horseshoe from '../../assets/img/iconHorseshoe.png';
+import * as lime from '../../assets/img/iconLime.png';
+import * as plum from '../../assets/img/iconPlum.png';
+import * as seven from '../../assets/img/iconSeven.png';
+import * as diamond from '../../assets/img/iconDiamond.png';
+import './component.scss';
 
 const api = new API();
 
@@ -17,6 +28,14 @@ export interface ReduxStateProps {
 
 type Props = PublicProps & ReduxStateProps & RouteProps;
 export class BroadcasterConfigPageComponent extends React.Component<Props, State> {
+  private onSubmitSlotName(event: React.FormEvent<HTMLFormElement>) {
+
+  }
+
+  private onSubmitImages(event: React.FormEvent<HTMLFormElement>) {
+    console.log(event.target);
+    console.log(event.currentTarget);
+  }
 
   public render() {
     if (!this.props.isBroadcaster) {
@@ -24,9 +43,76 @@ export class BroadcasterConfigPageComponent extends React.Component<Props, State
     }
 
     return (
-      <div>
+      <div className='broadcaster-page'>
+        <div className='slot machine title'>
+          <div>Customize the name of your slot machine</div>
+          <form onSubmit={e => this.onSubmitSlotName(e)} >
+            <input></input>
+          </form>
+        </div>
+        <div className='uploads'>
+          <div>
+            Upload Custom Slot Images:
+          <ul>
+              <li>Images will be transformed to be square, and won't look great if you upload a rectangular image.</li>
+              <li>Images dimensions must be no greater than 128px by 128px.</li>
+              <li>Images must be less than 25kb.</li>
+              <li>Accepted image formats are PNG, JPG, and JPEG only.</li>
+              <li>Image uploads should adhere to the <a href='https://help.twitch.tv/customer/en/portal/articles/2348985-partner-emoticon-and-badge-guide#guidelines'>Twitch's Subscriber Badge and Emoticon Guidelines</a>.</li>
+              <li>For general image upload concerns refer to: <a href='https://help.twitch.tv/customer/en/portal/articles/2348985-partner-emoticon-and-badge-guide#create'>Twitch's Subscriber Emoticon Guide </a>.</li>
+            </ul>
+          </div>
+          <form onSubmit={e => this.onSubmitImages(e)} >
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='1' />
+              <div><div>Replaces</div> <img className='image-preview' src={diamond} /></div>
+            </div>
 
-      </div>
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='2' />
+              <div><div>Replaces</div> <img className='image-preview' src={bar} /></div>
+            </div>
+
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='3' />
+              <div><div>Replaces</div> <img className='image-preview' src={bell} /></div>
+            </div>
+
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='4' />
+              <div><div>Replaces</div> <img className='image-preview' src={cherries} /></div>
+            </div>
+
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='5' />
+              <div><div>Replaces</div> <img className='image-preview' src={coin} /></div>
+            </div>
+
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='6' />
+              <div><div>Replaces</div> <img className='image-preview' src={horseshoe} /></div>
+            </div>
+
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='7' />
+              <div><div>Replaces</div> <img className='image-preview' src={seven} /></div>
+              <div className='seven-text'>Seven is special in Lucky Leaderboard. Getting 3 of these is the Jackpot score. Upload a special image for this one!</div>
+            </div>
+
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='8' />
+              <div><div>Replaces</div> <img className='image-preview' src={plum} /></div>
+            </div>
+
+            <div className='broadcaster-upload-container'>
+              <ImageUploader assetName='9' />
+              <div><div>Replaces</div> <img className='image-preview' src={lime} /></div>
+            </div>
+            <button className='submitButton'
+              type='submit'>Upload Images</button>
+          </form>
+        </div>
+      </div >
     );
   }
 }
